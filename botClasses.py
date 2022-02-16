105fdeb9142e950813f0c532a6dbd210f8af103f
תם <@TOM-LAP>
2022-02-16 16:19:58 +0200

    classes and register

---
+++ botClasses.py
@@ -0,0 +1,26 @@
+class Son:
+    def __init__(self, fname, lname, age, city, phone, elder):
+        super().__init__()
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        self.city = city
+        self.phone = phone
+        self.elder = elder
+
+
+class Pro:
+    def __init__(self, name, age, city, phone, profession):
+        super().__init__()
+        self.name = name
+        self.age = age
+        self.city = city
+        self.phone = phone
+        self.profession
+
+class Elder:
+    def __init__(self, name, city, phone):
+        super().__init__()
+        self.name = name
+        self.city = city
+        self.phone = phone    
